0e9adf1f4ef3551e5cdef5cd509e34ca6c7395e2
chaitumuppala <chaitanyamuppala9@gmail.com>
2024-12-23 19:48:56 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/hellohari/MainActivity.java
+++ android/app/src/main/java/com/hellohari/MainActivity.java
@@ -4,8 +4,25 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import android.content.pm.PackageManager;
+import android.Manifest;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Request permissions when activity is created
+        String[] permissions = {
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_PHONE_NUMBERS,
+            Manifest.permission.READ_CALL_LOG,
+            Manifest.permission.RECORD_AUDIO
+        };
+        ActivityCompat.requestPermissions(this, permissions, 1);
+    }
+
     @Override
     protected String getMainComponentName() {
         return "HelloHari";
