1a3ec644f798ea67db988c8a5f700b5d4643b70c
chaitumuppala <chaitanyamuppala9@gmail.com>
2024-12-24 08:45:59 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/hellohari/MainActivity.java
+++ android/app/src/main/java/com/hellohari/MainActivity.java
@@ -4,59 +4,8 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
-import android.os.Bundle;
-import android.content.pm.PackageManager;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import android.Manifest;
 
 public class MainActivity extends ReactActivity {
-    private static final int PERMISSION_REQUEST_CODE = 123;
-
-    private String[] REQUIRED_PERMISSIONS = new String[] {
-        Manifest.permission.READ_PHONE_STATE,
-        Manifest.permission.READ_CALL_LOG,
-        Manifest.permission.RECORD_AUDIO,
-        Manifest.permission.READ_PHONE_NUMBERS
-    };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        checkAndRequestPermissions();
-    }
-
-    private void checkAndRequestPermissions() {
-        boolean allPermissionsGranted = true;
-        for (String permission : REQUIRED_PERMISSIONS) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                allPermissionsGranted = false;
-                break;
-            }
-        }
-
-        if (!allPermissionsGranted) {
-            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSION_REQUEST_CODE);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            boolean allGranted = true;
-            for (int result : grantResults) {
-                if (result != PackageManager.PERMISSION_GRANTED) {
-                    allGranted = false;
-                    break;
-                }
-            }
-            if (!allGranted) {
-                checkAndRequestPermissions(); // Request again if not granted
-            }
-        }
-    }
-
     @Override
     protected String getMainComponentName() {
         return "HelloHari";
