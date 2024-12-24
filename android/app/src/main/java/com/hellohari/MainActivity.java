a066be832fbb0b7b28f02c96abb5d811918f5c82
chaitumuppala <chaitanyamuppala9@gmail.com>
2024-12-24 09:56:45 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/hellohari/MainActivity.java
+++ android/app/src/main/java/com/hellohari/MainActivity.java
@@ -1,11 +1,55 @@
 package com.hellohari;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
+    private static final int PERMISSION_REQUEST_CODE = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkAndRequestPermissions();
+    }
+
+    private void checkAndRequestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECORD_AUDIO},
+                    PERMISSION_REQUEST_CODE);
+        } else {
+            startRecordingService();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                         String[] permissions,
+                                         int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startRecordingService();
+            }
+        }
+    }
+
+    private void startRecordingService() {
+        Intent serviceIntent = new Intent(this, CallRecordingService.class);
+        startService(serviceIntent);
+    }
+
     @Override
     protected String getMainComponentName() {
         return "HelloHari";
