d81f3f8629c5e0d4f95132bbf0fc32d53e0babe3
chaitumuppala <chaitanyamuppala9@gmail.com>
2025-01-08 18:40:46 +0530

    Create CallReceiver.java

---
+++ android/app/src/main/java/com/hellohari/CallReceiver.java
@@ -0,0 +1,23 @@
+package com.hellohari;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class CallReceiver extends BroadcastReceiver {
+    private static final String TAG = "CallReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
+            String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+            if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                String number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                Log.d(TAG, "Incoming call from: " + number);
+                // We'll handle the call here once permissions are working
+            }
+        }
+    }
+}
